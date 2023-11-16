da6bff80030bf7ad958d46f8a3224cc2c27c554f
Bombearo <shekjaden@gmail.com>
2023-11-16 12:06:49 +0000

    Added connection to mongoDB

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv, find_dotenv
+import certifi
+
+#Setup ENV Vars
+load_dotenv(find_dotenv())
+
+#Setup MongoDB
+PWD = os.getenv("MONGO_PWD")
+connection_string = f"mongodb+srv://Jaden:{PWD}@bakerytreats.txkm7zv.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string, tlsCAFile=certifi.where())
+
+
+
 
 app = FastAPI()
 
@@ -10,4 +25,5 @@ class Item(BaseModel):
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
+
 
